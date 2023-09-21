5f280d16102fbab64237f5e8de2371dc311374cf
Sean Milfort <seanmilfort@me.com>
2023-09-20 21:49:16 -0400

    Updated About Me/Landing Page Section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,13 +1,16 @@
 import React from "react";
-import { Heading, VStack, Text, Image, Stack, Box,} from "@chakra-ui/react";
+import { Heading, VStack, Text, Image, Stack, Box, ListItem, UnorderedList,} from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 import profilePicture from '../images/seanmilfort_profile.jpg'
 
 const heading = "About Me";
-const aboutMe1 = "Hi, I’m Sean Milfort!";
-const aboutMe2 = "I am currently a System Administrator II at Rite Aid! I have a passion for looking at new technology and continuing to better myself, specifically in the mobile field. I keep myself up to date with all of the different operating systems (Windows, macOS, Linux, Android) and the newest technology trends.";
-const aboutMe3 = "Besides my passion of IT, I'm also involved in many different other activities. On the side, I'm a mobile DJ that plays at many different venues for a magnitude of crowds. I am also into gaming and have my own Twitch channel!";
-const aboutMe4 = "My current position in my organization allows me to test and utilize new technology to improve our work environment. With this position, I am looking at inefficient processes and problems that are currently implemented. Once identified, we look at re-mediating and improving them with new technology. Because of this, I have been able to think outside the box to find different solutions.";
+const aboutMe1 = "Hello! Welcome to my page! If you are new here, you may have found this page from either my resume/professional links or from my social media/content creation paths. Either way that you got here you are more than welcome!";
+const aboutMe2 = "I am an individual with a passion for technology. I take this passion for technology and apply it to the many parts of my life professionally and leisurely. The top two parts of technology that I enjoy are consumer electronics, specifically mobile devices and smart home automation, and software development, specifically front-end and mobile development. I am constantly getting myself into the latest tech and love showing the information to anyone willing to listen. My love for constantly learning about technology has brought me to the path of currently pursuing a PhD in Computer Science.";
+const aboutMe3 = "Professionally, I'm currently working as a System Administrator II at Rite Aid. With this role, I get to do a ton of Infrastructure work with our systems and have been specializing in Mobile Device Management (Android/iOS) on this front. I have also been starting to help out with our Software Development teams and starting to write code for them on the various projects that happen internally. On another professional front, I have also begun teaching at the college level. This desire to share my knowledge has brought me to this path.";
+const aboutMe4 = "Leisurely, I run a lot of side projects. Following my social media is the best way to keep up to date with all the things that I'm working on, but in case you were curious, here is a list of the side things that I do run:";
+const aboutMe5 = "Feel free to follow any of these pages to get a live look into the things that I'm learning and passionate about!";
+const aboutMe6 = "Besides my passion for technology, I’m also involved in some other side projects (I like to stay busy!). I have a passion for music that comes out as a DJ, specializing in Dance Music (EDM), and a passion for gaming that I bring out with streaming a couple of nights a week.";
+const aboutMe7 = "I'm a hard-working individual who likes to stay busy and see how much I can handle and learn. As long as I'm experiencing new things, I am feeling fulfilled.";
 
 const LandingSection = () => (
   <div id="AboutMe">
@@ -21,17 +24,27 @@ const LandingSection = () => (
       <Stack direction={["column", "row"]} spacing={8} alignItems={"center"}>
         <Box>
           <div data-aos="fade-right">
-            <VStack spacing={6} maxWidth="500px" text-align="center">
-              <Text >{aboutMe1}</Text>
+            <VStack spacing={4} maxWidth="500px" text-align="center">
+              <Text>{aboutMe1}</Text>
               <Text>{aboutMe2}</Text>
               <Text>{aboutMe3}</Text>
-              <Text>{aboutMe4}</Text>
+              <Text>
+                {aboutMe4}
+                <UnorderedList mt={2}>
+                    <ListItem><u><a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/@seanmilfort">YouTube Page</a></u></ListItem>
+                    <ListItem><u><a target="_blank" rel="noopener noreferrer" href="https://news.seanmilfort.com/">Newsletter</a></u></ListItem>
+                    <ListItem><u><a target="_blank" rel="noopener noreferrer" href="https://blog.seanmilfort.com/">Blog</a></u></ListItem>
+                </UnorderedList>
+              </Text>
+              <Text>{aboutMe5}</Text>
+              <Text>{aboutMe6}</Text>
+              <Text>{aboutMe7}</Text>
             </VStack>
           </div>
         </Box>
         <Box>
-          <div data-aos="fade-left">
-            <Image src={profilePicture} maxWidth="300px"/>
+          <div data-aos="fade-left" >
+            <Image src={profilePicture} maxWidth="500px" margin="auto"/>
           </div>
         </Box>
       </Stack>
--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -3,26 +3,25 @@ import FullScreenSection from "./FullScreenSection";
 
 const greeting = "Hello, I am Sean Milfort!";
 const bio1 = "System Administrator II";
-const bio2 = "Enjoys Development Work";
-const bio3 = "PhD Student";
+const bio2 = "Software Development";
+const bio3 = "Professor";
+const bio4 = "PhD Student";
+
 
   const LandingSection = () => (
   <div id="Home">
   <FullScreenSection >
     <VStack spacing={12} textAlign="center">
-      <Heading as='h4' size='3xl'>
-        {greeting}
-      </Heading>
+
+      <Heading as='h4' size='3xl'>{greeting}</Heading>
+
       <VStack spacing={4}>
-      <Heading as='h2' size='lg'>
-      {bio1}
-      </Heading>
-      <Heading as='h2' size='lg'>
-      {bio2}
-      </Heading>
-      <Heading as='h2' size='lg'>
-      {bio3}
-      </Heading>
+
+        <Heading as='h2' fontStyle={"italic"} size='lg'>{bio1}</Heading>
+        <Heading as='h2' fontStyle={"italic"} size='lg'>{bio2}</Heading>
+        <Heading as='h2' fontStyle={"italic"} size='lg'>{bio3}</Heading>
+        <Heading as='h2' fontStyle={"italic"} size='lg'>{bio4}</Heading>
+
       </VStack>
     </VStack>
   </FullScreenSection>

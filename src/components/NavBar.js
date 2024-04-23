3a212234482e7a566d5560cca251be813f7a8017
Sean Milfort <seanmilfort@me.com>
2024-04-23 18:03:18 -0400

    Deploy Menu Bar Changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -71,7 +71,7 @@ const MenuLinks = ({isOpen, toggle }) => {
         spacing={8}
         align="center"
         justify={["center", "space-between", "flex-end", "flex-end"]}
-        direction={["column", "row", "row", "row"]}
+        direction={["column", "column", "row", "row"]}
         pt={[4, 4, 0, 0]}
       >
         <MenuItem to="#Home" onClick={toggle}>Home</MenuItem>
